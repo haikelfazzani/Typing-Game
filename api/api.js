887208299eb764f2cc7a72986010e6fca1a1faed
Haikel Fazzani <haikel.fazzani@gmail.com>
2018-10-26 00:55:26 +0100

    fix speed ranking errors

--- api/api.js
+++ api/api.js
@@ -4,11 +4,10 @@ let timeLeft = (htmlFields , currentTimer , letterCounterSubject) =>
     let c = 0 , finish = false , letterCnt = 0 , timeSelected = currentTimer;
     letterCounterSubject.subscribe(data => {
         letterCnt = data;
-        if(timeSelected === 5) c =  Math.round(((letterCnt/5)*12) * 100) / 100;        
-        else if(timeSelected === 30) c =  Math.round(((letterCnt/5)*2) * 100) / 100;        
-        else if(timeSelected === 60) c =  Math.round((letterCnt/5) * 100) / 100;
-        else c = Math.round(((letterCnt/5)/2) * 100) / 100;
-        console.log(c , timeSelected , 'letter : ' + letterCnt)
+        if(timeSelected === 5) c =  calculSpeed(letterCnt , 12);        
+        else if(timeSelected === 30) c =  calculSpeed(letterCnt , 2);        
+        else if(timeSelected === 60) c =  calculSpeed(letterCnt , 1);
+        else c = calculSpeed(letterCnt , 2);
     });
     
     //console.log(letterCnt)
@@ -61,24 +60,36 @@ function resetFields(htmlFields , dashResult)
     htmlFields.userInput.focus();
 }
 
+function calculSpeed(letterCnt , timeSelected) {
+    if(timeSelected < 31) {
+        return Math.round(((letterCnt/5)*timeSelected) * 100) / 100;
+    }
+    return Math.round(((letterCnt/5)/timeSelected) * 100) / 100;
+}
+
 function keyNotCounted(keyCode) {                
     return [8 , 46 , 13 , 16].includes(keyCode)        
 }
 
 function checkSpeedRank(speed , speedResult) 
 {
-    if(speed < 12) {
+    if(speed < 23) {
         speedResult.textContent = 'Slow';
-        speedResult.style.backgroundColor = '#dc3545';        
+        speedResult.style.backgroundColor = '#dc3545';
     }
-    else if(speed > 11 && speed < 110) {
-        speedResult.textContent = 'good';
-        speedResult.style.backgroundColor = '#1e7e34'; 
+
+    if(speed > 22 && speed < 45){
+        speedResult.textContent = 'Average';
+        speedResult.style.backgroundColor = '#1e7e34';                        
     }
-    else {
-        speedResult.textContent = 'superb';
+    if(speed > 44 && speed < 65){
+        speedResult.textContent = 'Fast';
+        speedResult.style.backgroundColor = '#1e7e34';
+    }
+    if(speed > 64){
+        speedResult.textContent = 'Pro';
         speedResult.style.backgroundColor = '#ff5722';
-    }
+    }  
 }
 
 
